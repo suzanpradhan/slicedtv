594fe8f5074047823dfd3f08559543868f19ba27
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2020-11-26 12:33:38 +0545

    Create User Registration API View and Add POST method.
    Created a simple POST Method for User Registration. This is linked to UserRegistration Serializer.

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# External Import
+from rest_framework import generics, response, status
 
-# Create your views here.
+# Internal Import
+from . import serializers
+
+
+class UserRegistrationView(generics.GenericAPIView):
+    """ Generic API View for User Registration """
+
+    serializer_class = serializers.UserRegistrationSerializer
+
+    def post(self, request):
+        """ Post Request for User Registration """
+        user = request.data
+
+        # From UserRegistration Serializer
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+
+        return response.Response(user_data, status=status.HTTP_201_CREATED)
