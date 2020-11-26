e34174206a8e8f480530fa0b8919c406090caa32
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2020-11-26 12:58:58 +0545

    Add Email Credentials for Gmail in settings and create utils file to send email
    Nothing done for email just congigured email in the settings and crated a utils file to send the email.
    Have used environment variabeles in  email and password for privacy. While deploying  or running in different machine this should be consider.

--- backend/slicedtv/settings.py
+++ backend/slicedtv/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +136,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+# Email Settings
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_PORT = 587
