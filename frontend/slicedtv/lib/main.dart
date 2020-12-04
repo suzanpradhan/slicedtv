e4d8eb36c1b3862f6beea4e37fcb7ace56e4ad86
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2020-12-04 23:42:11 +0545

    Auth Provider

--- frontend/slicedtv/lib/main.dart
+++ frontend/slicedtv/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:slicedtv/src/screens/landing_page.dart';
+import 'package:slicedtv/src/screens/subscription/subscription_detail.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sliced TV',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LandingPage(),
-    );
+        title: 'Sliced TV',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SubscriptionDetail());
   }
 }
