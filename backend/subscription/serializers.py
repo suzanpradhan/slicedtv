7692442729501bafc9080329ad2281be1d53c625
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2020-12-08 13:19:35 +0545

    Update Subscription Model and API
    To get all subscription listed.
    /subs/get-all-subscriptions/.

--- backend/subscription/serializers.py
+++ backend/subscription/serializers.py
@@ -2,8 +2,9 @@ from subscription.models import Subscription
 from rest_framework import serializers
 
 
-class DefaultSubscription(serializers.ModelSerializer):
+class ListAllSubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
 
-        fields=["subscription_type","subscription_amount",]
+        fields = ["subscription_type",
+                  "subscription_amount", 'subscription_detail']
