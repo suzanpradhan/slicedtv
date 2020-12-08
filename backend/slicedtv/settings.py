8f3b05ca7de5bc578ae5d56d9c4a85b5b93a0a7b
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2020-12-08 11:19:21 +0545

    Remove All configuration of AWS
    Now the project doesn't use AWS.

--- backend/slicedtv/settings.py
+++ backend/slicedtv/settings.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'user',
     'djongo',
-    'decouple',
-    'storages',
     'subscription',
     #'episodes',
     #'movie',
@@ -166,22 +164,3 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
 }
 
-
-# AWS Configuration
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
-AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
-AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
-DEFAULT_FILE_STORAGE = 'slicedtv.backend.PublicMediaStorage'
-
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_STATIC_LOCATION = 'ratatic'
-
-
-AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
-DEFAULT_FILE_STORAGE = 'slicedtv.backend.PublicMediaStorage'
-
-AWS_PRIVATE_MEDIA_LOCATION = 'media/private'
-PRIVATE_FILE_STORAGE = 'slicedtv.backend.PrivateMediaStorage'
--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-from aws_test.models import uplod as aws_models
 from user.models import User
 
 admin.site.register(User)
-
-admin.site.register(aws_models)
