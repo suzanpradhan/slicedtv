ec4d95fe57f8a03340307c557b164816f3e4e277
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2020-12-21 12:36:34 +0545

    Using os.environ instead on config. They are same
    .env file should be like this:
    export DJANGO_SECRET_KEY=keyhere
    export EMAIL_USER=emailhere
    export EMAIL_PASS=passhere
    export FRONTEND_URL=urlhere

--- backend/slicedtv/settings.py
+++ backend/slicedtv/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -161,8 +161,8 @@ REST_FRAMEWORK = {
 # Email Settings
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 EMAIL_PORT = 587
 
 # SIMPLE_JWT
